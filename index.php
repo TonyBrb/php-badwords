e2c977018d8b686a12d088b329a2a60721959597
TonyBrb <anthonybarbagallo95@gmail.com>
2021-12-17 16:44:28 +0100

    stampa lunghezza stringa

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
-<?php 
-$paragrafo= 'Ciao come stai?';
+<?php
+$paragrafo = 'Ciao come stai?';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php-badwords</title>
 </head>
+
 <body>
   <h1>
-    Paragrafo: <?php echo $paragrafo;?>
+    Paragrafo: <?php echo $paragrafo; ?>
   </h1>
+
+  <h2>Lunghezza: <?php echo strlen($paragrafo); ?></h2>
 </body>
+
 </html>
